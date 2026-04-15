ce357988999000d6363e05be2ecdf09b3d55c374
ThomasD-cmd <ThomasD@innerflightproductions.com>
2026-04-15 16:51:36 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Innerflight",
+  description: "Innerflight website",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
